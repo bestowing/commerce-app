ed1de65c7a0d89254d02e9d598a1fcdb065a9d66
청수 <bestowing@naver.com>
2022-07-28 18:12:53 +0900

    test: 뷰모델 테스트 클래스 추가

---
+++ CommerceApp/CommerceAppViewModelTests/LikeViewModelTests.swift
@@ -0,0 +1,21 @@
+//
+//  LikeViewModelTests.swift
+//  CommerceAppViewModelTests
+//
+//  Created by 이청수 on 2022/07/28.
+//
+
+import RxSwift
+import XCTest
+
+final class LikeViewModelTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
