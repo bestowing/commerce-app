ff03c93edc85b9c03bb020dc15dcbced9e8bf5c7
청수 <bestowing@naver.com>
2022-07-28 16:10:06 +0900

    chore: DefaultHomeNavigator, DefaultLikeNavigator 생성

---
+++ CommerceApp/CommerceApp/Presentation/Scenes/Home/DefaultHomeNavigator.swift
@@ -0,0 +1,18 @@
+//
+//  DefaultHomeNavigator.swift
+//  CommerceApp
+//
+//  Created by 이청수 on 2022/07/28.
+//
+
+final class DefaultHomeNavigator: HomeNavigator {
+
+    // MARK: - init/deinit
+
+    deinit {}
+
+    // MARK: - methods
+
+    func toHome() {}
+
+}
---
+++ CommerceApp/CommerceApp/Presentation/Scenes/Like/DefaultLikeNavigator.swift
@@ -0,0 +1,18 @@
+//
+//  DefaultLikeNavigator.swift
+//  CommerceApp
+//
+//  Created by 이청수 on 2022/07/28.
+//
+
+final class DefaultLikeNavigator: LikeNavigator {
+
+    // MARK: - init/deinit
+
+    deinit {}
+
+    // MARK: - methods
+
+    func toLike() {}
+
+}
