d7938237111941723b8efd59ba27775303985384
청수 <bestowing@naver.com>
2022-07-28 15:50:42 +0900

    chore: Application 객체 생성

---
+++ CommerceApp/CommerceApp/Application/Application.swift
@@ -0,0 +1,23 @@
+//
+//  Application.swift
+//  CommerceApp
+//
+//  Created by 이청수 on 2022/07/28.
+//
+
+final class Application {
+
+    // MARK: properties
+
+    static let shared = Application()
+
+    // MARK: - init/deinit
+
+    private init() {}
+
+    // MARK: - methods
+
+    /// 앱의 시작점입니다
+    func start() {}
+
+}
