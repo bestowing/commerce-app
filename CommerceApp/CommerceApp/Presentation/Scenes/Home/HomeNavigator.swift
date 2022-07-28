bcb498c5c9fd175c0bab7f2751c811c7b25febdc
청수 <bestowing@naver.com>
2022-07-28 16:07:37 +0900

    chore: HomeNavigator, LikeNavigator 생성

---
+++ CommerceApp/CommerceApp/Presentation/Scenes/Home/HomeNavigator.swift
@@ -0,0 +1,12 @@
+//
+//  HomeNavigator.swift
+//  CommerceApp
+//
+//  Created by 이청수 on 2022/07/28.
+//
+
+protocol HomeNavigator {
+
+    func toHome()
+
+}
---
+++ CommerceApp/CommerceApp/Presentation/Scenes/Like/LikeNavigator.swift
@@ -0,0 +1,12 @@
+//
+//  LikeNavigator.swift
+//  CommerceApp
+//
+//  Created by 이청수 on 2022/07/28.
+//
+
+protocol LikeNavigator {
+
+    func toLike()
+
+}
