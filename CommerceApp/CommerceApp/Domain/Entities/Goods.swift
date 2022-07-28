8db4b0f1b88bce7c771dbd9007bea33a4c25ad43
청수 <bestowing@naver.com>
2022-07-28 16:17:47 +0900

    chore: Goods와 GoodsUsecase 생성

---
+++ CommerceApp/CommerceApp/Domain/Entities/Goods.swift
@@ -0,0 +1,18 @@
+//
+//  Goods.swift
+//  CommerceApp
+//
+//  Created by 이청수 on 2022/07/28.
+//
+
+struct Goods {
+
+    let id: Int
+    let name: String
+    let image: String
+    let actualPrice: Int
+    let price: Int
+    let isNew: Bool
+    let sellCount: Int
+
+}
---
+++ CommerceApp/CommerceApp/Domain/Usecases/GoodsUsecase.swift
@@ -0,0 +1,10 @@
+//
+//  GoodsUsecase.swift
+//  CommerceApp
+//
+//  Created by 이청수 on 2022/07/28.
+//
+
+protocol GoodsUsecase {
+
+}
