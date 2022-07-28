c227f0140b2e40e2d4dfb3c867d77b66eea649c7
청수 <bestowing@naver.com>
2022-07-28 17:57:19 +0900

    chore: Network 정의

--- CommerceApp/CommerceApp/Data/NetworkPlatform/Network/Network.swift
+++ CommerceApp/CommerceApp/Data/NetworkPlatform/Network/Network.swift
@@ -5,6 +5,14 @@
 //  Created by 이청수 on 2022/07/28.
 //
 
-final class Network {
+import RxSwift
+
+final class Network<T> {
+
+    // MARK: - methods
+
+    func getItems(_ path: String) -> Observable<T> {
+        return Observable.create { _ in return Disposables.create() }
+    }
 
 }
