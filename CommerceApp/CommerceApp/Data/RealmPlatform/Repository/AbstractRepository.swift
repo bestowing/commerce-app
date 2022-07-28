a3a52c263436b9a9e5cc895779e3bb131e303b8e
청수 <bestowing@naver.com>
2022-07-28 17:56:06 +0900

    chore: AbstractRepository 프로토콜 정의

--- CommerceApp/CommerceApp/Data/RealmPlatform/Repository/AbstractRepository.swift
+++ CommerceApp/CommerceApp/Data/RealmPlatform/Repository/AbstractRepository.swift
@@ -5,6 +5,13 @@
 //  Created by 이청수 on 2022/07/28.
 //
 
+import RxSwift
+
 protocol AbstractRepository {
+
+    associatedtype T
+
+    func queryAll() -> Observable<[T]>
+    func save(_ entity: T) -> Observable<Void>
 
 }
--- CommerceApp/CommerceApp/Data/RealmPlatform/Repository/RealmRepository.swift
+++ CommerceApp/CommerceApp/Data/RealmPlatform/Repository/RealmRepository.swift
@@ -5,6 +5,16 @@
 //  Created by 이청수 on 2022/07/28.
 //
 
-final class RealmRepository: AbstractRepository {
+import RxSwift
+
+final class RealmRepository<T>: AbstractRepository {
+
+    func queryAll() -> Observable<[T]> {
+        return Observable.create { _ in return Disposables.create() }
+    }
+
+    func save(_ entity: T) -> Observable<Void> {
+        return Observable.create { _ in return Disposables.create() }
+    }
 
 }
