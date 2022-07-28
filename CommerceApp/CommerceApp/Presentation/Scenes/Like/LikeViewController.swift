465f610d403d33b02c0a8d7cccce58584be3913d
청수 <bestowing@naver.com>
2022-07-28 16:00:50 +0900

    chore: LikeViewController 생성

---
+++ CommerceApp/CommerceApp/Presentation/Scenes/Like/LikeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LikeViewController.swift
+//  CommerceApp
+//
+//  Created by 이청수 on 2022/07/28.
+//
+
+import UIKit
+
+final class LikeViewController: UIViewController {
+
+    // MARK: - init/deinit
+
+    deinit {}
+
+    // MARK: - methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
