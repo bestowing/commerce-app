01bbc518e0874b3d8d937bcb0c806859c7d5a6f2
청수 <bestowing@naver.com>
2022-07-29 02:26:32 +0900

    feat: 홈 화면에 collectionView 추가

--- CommerceApp/CommerceApp/Presentation/Scenes/Home/HomeViewController.swift
+++ CommerceApp/CommerceApp/Presentation/Scenes/Home/HomeViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 이청수 on 2022/07/28.
 //
 
+import SnapKit
 import UIKit
 
 final class HomeViewController: BaseViewController {
@@ -12,6 +13,12 @@ final class HomeViewController: BaseViewController {
     // MARK: - properties
 
     var viewModel: HomeViewModel!
+
+    private let collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        return collectionView
+    }()
 
     // MARK: - init/deinit
 
@@ -23,6 +30,15 @@ final class HomeViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "홈"
+        self.setSubViews()
+    }
+
+    private func setSubViews() {
+        self.view.addSubview(self.collectionView)
+        self.collectionView.snp.makeConstraints {
+            $0.edges.equalTo(self.view.safeAreaLayoutGuide)
+        }
     }
 
 }
