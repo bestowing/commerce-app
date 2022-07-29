a5d197437ff5367c1f24d0cfaa1a4620c29539e2
청수 <bestowing@naver.com>
2022-07-29 22:38:53 +0900

    feat: PaddingLabel 생성

---
+++ CommerceApp/CommerceApp/Presentation/Common/PaddingLabel.swift
@@ -0,0 +1,30 @@
+//
+//  PaddingLabel.swift
+//  CommerceApp
+//
+//  Created by 이청수 on 2022/07/29.
+//
+
+import UIKit
+
+final class PaddingLabel: UILabel {
+
+    private var edgeInsets: UIEdgeInsets = UIEdgeInsets()
+
+    convenience init(padding: UIEdgeInsets) {
+        self.init()
+        self.edgeInsets = padding
+    }
+
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: self.edgeInsets))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.width += self.edgeInsets.left + self.edgeInsets.right
+        contentSize.height += self.edgeInsets.top + self.edgeInsets.bottom
+        return contentSize
+    }
+
+}
