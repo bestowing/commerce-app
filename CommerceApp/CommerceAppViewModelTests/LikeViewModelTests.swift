e42676536153a664c0b1220144245aa9aa644daa
청수 <bestowing@naver.com>
2022-08-01 05:50:27 +0900

    test: LikeViewModelTests 테스트 추가를 위해 설정 완료

--- CommerceApp/CommerceAppViewModelTests/LikeViewModelTests.swift
+++ CommerceApp/CommerceAppViewModelTests/LikeViewModelTests.swift
@@ -6,16 +6,58 @@
 //
 
 import RxSwift
+import RxTest
 import XCTest
 
 final class LikeViewModelTests: XCTestCase {
 
+    // MARK: - properties
+
+    private var mockLikeNavigator: MockLikeNavigator!
+    private var mockLikeUsecase: MockLikeUsecase!
+    private var viewModel: LikeViewModel!
+    private var disposeBag: DisposeBag!
+    private var scheduler: TestScheduler!
+
+    // MARK: - methods
+
     override func setUp() {
         super.setUp()
+        self.mockLikeNavigator = MockLikeNavigator()
+        self.mockLikeUsecase = MockLikeUsecase()
+        self.viewModel = LikeViewModel(
+            navigator: self.mockLikeNavigator,
+            likeUsecase: self.mockLikeUsecase
+        )
+        self.disposeBag = DisposeBag()
+        self.scheduler = TestScheduler(initialClock: 0)
     }
 
     override func tearDown() {
+        mockLikeNavigator = nil
+        mockLikeUsecase = nil
+        viewModel = nil
+        disposeBag = nil
+        scheduler = nil
         super.tearDown()
+    }
+
+}
+
+extension LikeViewModelTests {
+
+    final class MockLikeNavigator: LikeNavigator {
+        func toLike() {}
+    }
+
+    final class MockLikeUsecase: LikeUsecase {
+
+        var likeGoodsStream = PublishSubject<[Goods]>()
+
+        func likeGoods() -> Observable<[Goods]> {
+            return self.likeGoodsStream.asObservable()
+        }
+        
     }
 
 }
