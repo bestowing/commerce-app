0a5dab291e6265a82ddce9412e8503ff634ae66e
청수 <bestowing@naver.com>
2022-08-01 19:51:37 +0900

    test: 초기 좋아요가 없는 경우 테스트 구현

--- CommerceApp/CommerceAppViewModelTests/HomeViewModelTests.swift
+++ CommerceApp/CommerceAppViewModelTests/HomeViewModelTests.swift
@@ -175,67 +175,58 @@ final class HomeViewModelTests: QuickSpec {
                     }
                     context("홈 섹션 모델 배열을 방출할때") {
                         it("상품 섹션의 아이템들에서 모든 데이터의 isLiked가 false여야 한다") {
-                            
-                        }
-                    }
-                    context("추가로 요청했을때") {
-                        context("이전에 요청했던 결과가 길이가 0인 배열이라면") {
-                            it("유즈케이스로 요청을 보내지 말아야한다") {
-                                
+                            let testGoods = (0..<10).map {
+                                Goods(
+                                    id: $0, name: "", image: "", isNew: false,
+                                    sellCount: 0, actualPrice: 0, price: 0
+                                )
                             }
-                        }
-                        context("이전에 요청했던 결과가 비어있지 않았고") {
-                            context("추가로 받아오는 데이터가 길이가 0인 배열이라면") {
-                                it("현재까지의 데이터가 그대로 방출되어야 한다") {
-                                    
+                            let testInitialValues: ([Banner], [Goods]) = ([], testGoods)
+                            let expected = Array(repeating: false, count: testGoods.count)
+                            mockHomeUsecase.getLikesGoodsStream = scheduler.createHotObservable([
+                                .next(255, [Goods]())
+                            ]).asObservable()
+                            mockHomeUsecase.initializationStream = scheduler.createHotObservable([
+                                .next(250, testInitialValues)
+                            ]).asObservable()
+                            let testViewDidLoadTrigger = scheduler.createHotObservable(
+                                [.next(240, ())]
+                            ).asDriverOnErrorJustComplete()
+                            let input = HomeViewModel.Input(
+                                viewDidLoad: testViewDidLoadTrigger,
+                                loadMore: Driver<Void>.never(),
+                                refresh: Driver<Void>.never(),
+                                like: Driver<GoodsItemViewModel>.never()
+                            )
+                            let output = viewModel.transform(input: input)
+
+                            let testableObserver = scheduler.createObserver([Bool].self)
+                            output.homeSectionModels
+                                .compactMap { models -> [Bool]? in
+                                    switch models[1] {
+                                    case let .GoodsSection(items):
+                                        return items.compactMap {
+                                            switch $0 {
+                                            case let .GoodsSectionItem(itemViewModel):
+                                                return itemViewModel.isLiked
+                                            default:
+                                                return nil
+                                            }
+                                        }
+                                    default:
+                                        return nil
+                                    }
                                 }
-                            }
-                            context("추가로 받아오는 데이터가 길이가 1인 배열이라면") {
-                                it("현재까지의 데이터의 상품 섹션에 길이가 1인 배열을 추가하여 방출되어야 한다") {
-                                    
-                                }
-                            }
-                            context("추가로 받아오는 데이터가 길이가 10인 배열이라면") {
-                                it("현재까지의 데이터의 상품 섹션에 길이가 10인 배열을 추가하여 방출되어야 한다") {
-                                    
-                                }
-                            }
-                        }
-                    }
-                    context("리프레시를 했을때") {
-                        it("초기값으로 섹션 모델을 만들어 방출해야 한다") {
-                            
-                        }
-                    }
-                    context("하트모양 버튼을 눌렀을때") {
-                        context("좋아요하지 않았던 데이터였다면") {
-                            it("선택한 데이터의 좋아요를 생성하는 요청을 유즈케이스로 보내야한다") {
-                                
-                            }
-                        }
-                        context("이미 좋아요한 데이터였다면") {
-                            it("선택한 데이터의 좋아요를 삭제하는 요청을 유즈케이스로 보내야한다") {
-                                
-                            }
-                        }
-                    }
-                }
-                context("이전에 좋아요한 데이터가 하나 이상 있을때") {
-                    context("홈 섹션 모델 배열을 방출할때") {
-                        context("배열 안에 이전에 좋아한 데이터가 없다면") {
-                            it("상품 섹션의 아이템들에서 모든 데이터의 isLiked가 false여야 한다") {
-                                
-                            }
-                        }
-                        context("배열 안에 이전에 좋아한 데이터가 1개라면") {
-                            it("상품 섹션의 아이템들에서 좋아요한 데이터만 isLiked가 true여야 한다") {
-                                
-                            }
-                        }
-                        context("배열 안에 모든 데이터를 이전에 좋아했었다면") {
-                            it("상품 섹션의 모든 아이템들에서 isLiked가 true여야 한다") {
-                                
-                            }
+                                .drive(testableObserver)
+                                .disposed(by: disposeBag)
+                            output.events
+                                .drive()
+                                .disposed(by: disposeBag)
+
+                            scheduler.start()
+                            XCTAssertEqual(testableObserver.events, [
+                                .next(255, expected)
+                            ])
                         }
                     }
                 }
